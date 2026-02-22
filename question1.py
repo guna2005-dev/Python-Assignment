0dc8ca43b73b8d0e376f886a5693c33a29b0cedf
guna2005-dev <gunavathihr123@gmail.com>
2026-02-22 11:42:40 +0530

    Add files via upload

---
+++ question1.py
@@ -0,0 +1,17 @@
+def question1():
+    name = "Guna gowda"
+    age = 20
+    course = "Python Programming"
+    college = "AIEMS"
+    email = "gunavthihr123@gmail.com"
+
+    print("=" * 40 )
+    print("|    STUDENT BIO CARD    ")
+    print("=" * 40 )
+    print(f"| Name: {name}")
+    print(f"| Age: {age}")
+    print(f"| Course: {course}")
+    print(f"| College: {college}")
+    print(f"| Email: {email}")
+    print("=" * 40 )
+question1()
